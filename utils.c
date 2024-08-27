a99b3ab6174477fbb735b2bbedf186e5d72384e4
Yang Wang <yawang@made-f0Ar2s9.clusters.42paris.fr>
2024-08-27 18:19:20 +0200

    create get position function

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: yawang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 14:25:25 by yawang            #+#    #+#             */
-/*   Updated: 2024/08/20 14:28:00 by yawang           ###   ########.fr       */
+/*   Updated: 2024/08/27 18:15:39 by yawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,31 @@ size_t	ft_arrlen(const char **arr)
 	while (arr[i])
 		i++;
 	return (i);
+}
+
+t_pxy	*get_position(char **map, char component)
+{
+	t_pxy	*position;
+	size_t	i;
+	size_t	j;
+
+	position = malloc(sizeof(t_pxy));
+	if (!position)
+		return (NULL);
+	i = -1;
+	while (map[++i])
+	{
+		j = -1;
+		while (map[i][++j])
+		{
+			if (map[i][j] == component)
+			{
+				position->y = i;
+				position->x = j;
+				return (position);
+			}
+		}
+	}
+	free(position);
+	return (NULL);
 }
